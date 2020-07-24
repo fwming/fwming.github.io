2e6d057bb35e0d09891dd37277f5bd52a3be1894
jesper <1576852932@qq.com>
2020-07-24 18:53:33 +0800

    update json/notes/table/data.js.

---
+++ json/notes/table/data.js
@@ -0,0 +1,26 @@
+{
+	"code": 0,
+	"msg": "",
+	"count": 100,
+	"data": [{
+		"id": "10001",
+		"url": "http://www.baidu.com",
+		"status": "200"
+	},{
+		"id": "10002",
+		"url": "https://blog.csdn.net/qq_29722281/artie",
+		"status": "200"
+	},{
+		"id": "10003",
+		"url": "https://blog.csdn.net/xi_weina/article/details/51242845",
+		"status": "500"
+	},{
+		"id": "10004",
+		"url": "https://www.layui.com/demo/table/toolbar.html",
+		"status": "404"
+	},{
+		"id": "10005",
+		"url": "dist/views/conventional/notes/router/index.html",
+		"status": "404"
+	}]
+}
