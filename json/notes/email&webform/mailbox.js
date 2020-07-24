7af2491890710686c664159efb8e3da98279ab19
jesper <1576852932@qq.com>
2020-07-24 18:52:35 +0800

    add json/notes/email&webform/mailbox.js.

---
+++ json/notes/email&webform/mailbox.js
@@ -0,0 +1,84 @@
+{
+	"code": 0,
+	"msg": "",
+	"count": "auto",
+	"data": [{
+		"id": "10001",
+		"type": "gamil",
+		"email": "chiumansuet@gmail.com",
+		"who": "Hazel",
+		"system": "IOS"
+	},{
+		"id": "10002",
+		"type": "gamil",
+		"email": "1124wasd@gmail.com",
+		"who": "Eliot",
+		"system": "Android"
+	},{
+		"id": "10003",
+		"type": "outlook",
+		"email": "weiming.fang@xgate.com",
+		"who": "Jesper",
+		"system": "2016"
+	},{
+		"id": "10004",
+		"type": "outlook",
+		"email": "xander.chen@xgate.com",
+		"who": "Xander",
+		"system": "2010"
+	},{
+		"id": "10005",
+		"type": "yahoo",
+		"email": "katherineleungmm@yahoo.com",
+		"who": "Katherine",
+		"system": "IOS"
+	},{
+		"id": "10006",
+		"type": "163",
+		"email": "only1quincy@163.com",
+		"who": "Quincy",
+		"system": "IOS"
+	},{
+		"id": "10007",
+		"type": "163",
+		"email": "13570341201@163.com",
+		"who": "Fanny",
+		"system": "Android"
+	},{
+		"id": "10008",
+		"type": "163",
+		"email": "cvbfxgcrty@163.com",
+		"who": "Eliot",
+		"system": "Android"
+	},{
+		"id": "10009",
+		"type": "QQ",
+		"email": "358302395@qq.com",
+		"who": "Jin",
+		"system": "IOS"
+	},{
+		"id": "10010",
+		"type": "QQ",
+		"email": "1456759643@qq.com",
+		"who": "Alisa",
+		"system": "Android"
+	},{
+		"id": "10011",
+		"type": "QQ",
+		"email": "825205700@qq.com",
+		"who": "Dee",
+		"system": "Android"
+	},{
+		"id": "10012",
+		"type": "hotmail",
+		"email": "xgedmtest@hotmail.com",
+		"who": "XGATE",
+		"system": "---"
+	},{
+		"id": "10013",
+		"type": "sohu",
+		"email": "1201794629285040128@sohu.com",
+		"who": "Eliot",
+		"system": "Android"
+	}]
+}
